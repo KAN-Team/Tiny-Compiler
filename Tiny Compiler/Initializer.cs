9d19df1370bbf80f1f6f0c9d88c9cdbd13c64d32
Abanoub-Asaad <abanoub.asaad@robustastudio.com>
2021-11-13 00:54:13 -0800

    feat: add more reserved words

--- Tiny Compiler/Initializer.cs
+++ Tiny Compiler/Initializer.cs
@@ -20,8 +20,22 @@ namespace Tiny_Compiler
             reservedWords.Add("End", "end-stmt");
             reservedWords.Add("Repeat", "repeat-stmt");
             reservedWords.Add("Until", "until-stmt");
+            reservedWords.Add("EndUntil", "end until-stmt");
             reservedWords.Add("Read", "Read-input-stmt");
             reservedWords.Add("Write", "Write-output-stmt");
+            reservedWords.Add("Call", "call-stmt");
+            reservedWords.Add("Declare", "declare-stmt");
+            reservedWords.Add("Begin", "begin-stmt");
+            reservedWords.Add("Do", "do-stmt");
+            reservedWords.Add("EndIf", "end if-stmt");
+            reservedWords.Add("While", "while-stmt");
+            reservedWords.Add("EndWhile", "end while-stmt");
+            reservedWords.Add("Set", "set-stmt");
+            reservedWords.Add("Real", "real-data type");
+            reservedWords.Add("Integer", "integer-data type");
+            reservedWords.Add("Program", "program-stmt");
+            reservedWords.Add("Procedure", "procedure-stmt");
+            reservedWords.Add("Parameters", "parameters-stmt");
 
             specialSymbols.Add("+", "Addition-symbol");
             specialSymbols.Add("-", "Subtraction-symbol");
@@ -34,6 +48,8 @@ namespace Tiny_Compiler
             specialSymbols.Add("<=", "Smaller or Equal-symbol");
             specialSymbols.Add("!=", "Not equal-symbol");
             specialSymbols.Add(";", "Semicolon symbol");
+            specialSymbols.Add(",", "Comma symbol");
+            specialSymbols.Add(".", "Dot symbol");
             specialSymbols.Add("(", "Left Parenthesis");
             specialSymbols.Add(")", "Right Parenthesis");
         }
